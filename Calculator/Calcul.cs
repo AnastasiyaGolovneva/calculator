8fbee26eb55db80f698263f859a9ffa257e812fa
AnastasiyaGolovneva <asya-gol@mail.ru>
2014-04-02 18:37:52 +0600

    add test project and first test method

---
+++ Calculator/Calcul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calcul
+    {
+        protected double data;
+
+        public Calcul()
+        {
+            data = 0;            
+        }
+        public double getData()
+        {
+            return 0;
+        }
+    }
+}
---
+++ CalculatorTest/CalculTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Calculator;
+
+namespace CalculatorTest
+{
+    /// <summary>
+    /// Summary description for CalculTest
+    /// </summary>
+    [TestClass]
+    public class CalculTest
+    {
+        public CalculTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Calctlnit()
+        {
+            Calcul newCalcul = new Calcul();
+            double expected = 0;
+            double actual = newCalcul.getData();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
